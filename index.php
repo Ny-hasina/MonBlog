bc2729c2cfde44f0dd5d4ea1ab48ab546598361b
Ny Hasina <adekanyhasina@gmail.com>
2025-11-21 16:12:37 +0100

    dernieres modif

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <div id="global">
             <header>
                 <a href="index.php"><h1 id="titreBlog">Mon Blog</h1></a>
-                <p>Je vous souhaite la bienvenue sur ce modeste blog.</p>
+                <p>Bienvenue sur ce modeste blog.</p>
             </header>
             <div id="contenu">
                 <?php
